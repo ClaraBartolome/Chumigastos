2aa8f0d2d97323f1e41b49e2704c66d9387390f8
Clara Bartolome Pereira <patatita300@gmail.com>
2024-04-13 02:57:26 +0200

    Added pass from eur to yen
    Added textfield in which user can enter digits and it woud be calculated how much yens or eurs that is

--- app/src/main/java/com/example/myapplication/compose/components/MoneyItem.kt
+++ app/src/main/java/com/example/myapplication/compose/components/MoneyItem.kt
@@ -1,54 +1,55 @@
 package com.example.myapplication.compose.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.R
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MoneyItem() {
+fun MoneyItem(digit: MutableState<Double>, icon: Int = R.drawable.ic_euro, onUpdateNumber: () -> Unit) {
+    var text by remember { mutableStateOf(formatText(digit.value)) }
+    var previousValue = remember { mutableStateOf(digit.value) }
+    LaunchedEffect(digit.value) {
+        // Se ejecutará cuando el valor de digit cambie
+        text = formatText(digit.value)
+    }
     Card(
         modifier = Modifier
             .padding(horizontal = 16.dp, vertical = 8.dp)
@@ -69,12 +70,18 @@ fun MoneyItem() {
             .padding(start = 16.dp, top = 8.dp, bottom = 8.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically){
             Column(modifier = Modifier
                 .weight(1f)) {
-
-                //TODO FALTA TERMINAR DE CAPAR QUE SOLO ADMITA NÚMEROS Y MAX CARACTERES
                 OutlinedTextField(
-                    value = "String",
-                    onValueChange = {},
-                    textStyle = MaterialTheme.typography.bodyMedium,
+                    value = text,
+                    onValueChange = {
+                        val newText = it.text.replace(Regex("[^\\d.]"), "")
+                        text = TextFieldValue(newText, it.selection)
+                        if(previousValue.value != parseValue(it.text)){
+                            previousValue.value = parseValue(it.text)
+                            digit.value = parseValue(it.text)
+                            onUpdateNumber.invoke()
+                        }
+                    },
+                    textStyle = MaterialTheme.typography.titleMedium,
                     singleLine = true,
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     colors = OutlinedTextFieldDefaults.colors(
@@ -94,7 +101,7 @@ fun MoneyItem() {
 
             Column(modifier = Modifier
                 .padding(horizontal = 8.dp)){
-                Image(painter = painterResource(id = R.drawable.ic_euro), contentDescription = "", modifier = Modifier
+                Image(painter = painterResource(id = icon), contentDescription = "", modifier = Modifier
                     .size(32.dp), colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
                 )
             }
@@ -102,10 +109,41 @@ fun MoneyItem() {
     }
 }
 
+
+private fun formatText(value: Double): TextFieldValue {
+    val formattedValue = if (value % 1 == 0.0) {
+        value.toInt().toString()
+    } else {
+        String.format(Locale.getDefault(), "%.2f", value)
+    }
+    return TextFieldValue(formattedValue, TextRange(formattedValue.length))
+}
+
+private fun parseValue(text: String): Double {
+    val formattedText = text.replace(Regex("[^\\d.]"), "")
+    val parsedValue = if (formattedText.isNotEmpty()) {
+        try {
+            formattedText.toDouble()
+        } catch (e: NumberFormatException) {
+            0.0
+        }
+    } else {
+        0.0
+    }
+    return if (parsedValue % 1 == 0.0) parsedValue.toInt().toDouble() else parsedValue
+}
+
 @Preview(showBackground = true, showSystemUi = true, apiLevel = 33)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        MoneyItem()
+        Column {
+            MoneyItem(
+                remember { mutableStateOf<Double>(1.0) },
+                R.drawable.ic_euro, {})
+            MoneyItem(
+                remember { mutableStateOf<Double>(1.0) },
+                R.drawable.ic_yen, {})
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/compose/screens/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/compose/screens/MainScreen.kt
@@ -1,28 +1,58 @@
 package com.example.myapplication.compose.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
+import com.example.myapplication.compose.components.MoneyItem
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 @Composable
 fun MainScreen() {
-    Text(
-        text = "Hello Android"
-    )
+    LazyColumn(){
+        item {
+            Column(verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.padding(top = 64.dp)) {
+                val numberEur = remember { mutableStateOf<Double>(1.0) }
+                val numberYen = remember { mutableStateOf<Double>(1.0) }
+                MoneyItem(numberEur, R.drawable.ic_euro) { numberYen.value = onUpdateYen(numberEur) }
+                Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+                    Image(painter = painterResource(id = R.drawable.ic_arrow), contentDescription = "", modifier = Modifier
+                        .size(32.dp), colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
+                    )
+                    Image(painter = painterResource(id = R.drawable.ic_arrow), contentDescription = "", modifier = Modifier
+                        .size(32.dp), colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
+                    )
+                }
+                MoneyItem(numberYen, R.drawable.ic_yen) { numberEur.value = onUpdateEur(numberYen) }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+fun onUpdateEur(yen: MutableState<Double>) = yen.value / 164
+
+fun onUpdateYen(eur: MutableState<Double>) = eur.value * 164
+
+@Preview(showBackground = true, showSystemUi = true, apiLevel = 33)
 @Composable
 fun prevMainScreen(){
     MyApplicationTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            MainScreen()
-        }
+        MainScreen()
     }
 }
