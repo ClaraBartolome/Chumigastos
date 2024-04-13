9e3a609cac1185dcf87ca20403e7339c9b4deb78
Clara Bartolome Pereira <patatita300@gmail.com>
2024-04-14 01:56:35 +0200

    Added background image

--- app/src/main/java/com/example/myapplication/compose/screens/UICompose.kt
+++ app/src/main/java/com/example/myapplication/compose/screens/UICompose.kt
@@ -2,6 +2,8 @@ package com.example.myapplication.compose.screens
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -10,10 +12,14 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.R
 import com.example.myapplication.common.ChumiScreens
 import com.example.myapplication.compose.components.AlertExchange
 import com.example.myapplication.compose.components.TopAppBarDefault
@@ -39,33 +45,43 @@ fun UICompose(
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(route = ChumiScreens.Start.name) {
-                MainScreen(
-                    yenExchange,
-                    eurExchange,
-                    onClickAdd = { createToast(ctx) },
-                    onClickList = { createToast(ctx) },
-                    onClickTotals = { createToast(ctx) }
-                )
-                if (isPopUpExchangeOpen.value) {
-                    AlertExchange(
-                        eurExchange.value,
-                        yenExchange.value,
-                        onDismissRequest = {
+                Box(
+                    modifier = with (Modifier){
+                        fillMaxSize()
+                            .paint(
+                                // Replace with your image id
+                                painterResource(id = R.drawable.bg_image_test),
+                                contentScale = ContentScale.FillBounds)
+
+                    }) {
+                    MainScreen(
+                        yenExchange,
+                        eurExchange,
+                        onClickAdd = { createToast(ctx) },
+                        onClickList = { createToast(ctx) },
+                        onClickTotals = { createToast(ctx) }
+                    )
+                    if (isPopUpExchangeOpen.value) {
+                        AlertExchange(
+                            eurExchange.value,
+                            yenExchange.value,
+                            onDismissRequest = {
+                                isPopUpExchangeOpen.value = false
+                            }) { text, exchangeEurYen ->
+                            if (text.isNotBlank() && exchangeEurYen) {
+                                eurExchange.value = text.toFloat()
+                                yenExchange.value = 1/eurExchange.value
+                                saveEurExchange.invoke()
+                            } else if (!exchangeEurYen) {
+                                yenExchange.value = text.toFloat()
+                                eurExchange.value = 1/yenExchange.value
+                                saveYenExchange.invoke()
+                            }
                             isPopUpExchangeOpen.value = false
-                        }) { text, exchangeEurYen ->
-                        if (text.isNotBlank() && exchangeEurYen) {
-                            eurExchange.value = text.toFloat()
-                            yenExchange.value = 1/eurExchange.value
-                            saveEurExchange.invoke()
-                        } else if (!exchangeEurYen) {
-                            yenExchange.value = text.toFloat()
-                            eurExchange.value = 1/yenExchange.value
-                            saveYenExchange.invoke()
                         }
-                        isPopUpExchangeOpen.value = false
                     }
                 }
-            }
+                }
         }
     }
 }
